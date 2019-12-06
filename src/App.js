e4b141e303c71b3e732061c52706cdc6db65417b
Lbenedettomt <luigibenedettomt@gmail.com>
2019-12-06 16:10:56 -0300

    New implementations of weatherAPI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Titles from "./Components/Titles";
+import Form from "./Components/Form";
+import Weather from "./Components/Weather";
+
+const API_KEY = "e1a227aed0d79b40694fc5cfd15529c1"
 
 class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data); 
+  }
   render() {
     return (
       <div>
-        <Titles/>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
       </div>
     );
   }
