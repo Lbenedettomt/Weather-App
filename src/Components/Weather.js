6a2da7837a32fec56ba952f2d72d832136f304f9
Lbenedettomt <luigibenedettomt@gmail.com>
2019-12-10 16:01:46 -0300

    Showing API results and importing css styles

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-class Weather extends React.Component {
-    render() {
-        return(
-            <div>Weather Component</div>
-        );
-    }
-};
+const Weather = props => (
+  <div>
+    {props.city && props.country && <p>Localização: {props.city}, {props.country}</p>}
+    {props.temperature && <p>Temperatura: {props.temperature}</p>}               
+    {props.humidity && <p>Umidade: {props.humidity}</p>}
+    {props.description && <p>Condições: {props.description}</p>} 
+    {props.error && <p>{props.error}</p>}
+  </div>
+);
 
 export default Weather;
